0429622439b85e2d421de98995cac55b9d129e43
Nitesh Talreja <niteshtalreja26@gmail.com>
2026-08-05 00:52:51 +0530

    FIX: Comment out EmailService temporarily for deployment

--- src/main/java/com/skillswap/service/EmailService.java
+++ src/main/java/com/skillswap/service/EmailService.java
@@ -1,4 +1,4 @@
-package com.skillswap.service;
+/*package com.skillswap.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
@@ -54,3 +54,5 @@ public class EmailService {
         mailSender.send(message);
     }
 }
+
+ */
